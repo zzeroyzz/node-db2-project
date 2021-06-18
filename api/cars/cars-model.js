5e3e0969fe29fda5afa98f0c74fede8d7386e415
kato hawkins <kristhahawkins@icloud.com>
2021-06-18 13:39:59 -0400

    completed cars-model.js

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,22 @@
+const db = require("../../data/db-config.js")
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('cars')
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = id => {
+  return db('cars').where('id',id).first()
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+const create = async car => {
+  return db('cars').insert(car)
+  .then(([id]) =>{
+    return db('cars').where('id',id).first()
+  })
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create,
 }
