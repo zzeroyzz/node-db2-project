998962bab662e60d5af7270984f0a42972363c18
kato hawkins <kristhahawkins@icloud.com>
2021-06-18 13:26:44 -0400

    20210618171310_01-make_cars_table.js completed

--- data/migrations/20210618171310_01-make_cars_table.js
+++ data/migrations/20210618171310_01-make_cars_table.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("cars", table =>{
+    table.increments()//default id
+    table.text("vin",128).unique().notNullable()//vin column vin needs to be unique and require(notnullable)
+    table.text("make",128)
+    table.text("model",128)
+    table.integer("mileage")
+    table.text("title",128)
+    table.text("transmission",128)
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("cars")
 };
